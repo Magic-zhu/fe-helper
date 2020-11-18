2bf6961bc257da1fb92df0dd88890b058fc782bb
magic-zhu <18817313320@163.com>
2020-11-18 17:03:19 +0800

    feat:npm源切换

---
+++ command/npm.js
@@ -0,0 +1,36 @@
+const child = require('child_process');
+const inquirer = require('inquirer');
+const spinner = require('ora')();
+module.exports = function(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'npm_origin',
+            message: "选择镜像源?",
+            choices: [
+                "npm原始",
+                "淘宝",
+                "得物"
+            ]
+        }
+    ]).then(answers=>{
+        if(answers.npm_origin=='npm原始'){
+            child.exec('npm config set registry http://registry.npmjs.org/',(err,data)=>{
+                if(!err) spinner.succeed('设置成功')
+                else spinner.fail(err);
+            })
+        }
+        if(answers.npm_origin=='淘宝'){
+            child.exec('npm config set registry http://registry.npm.taobao.org/',(err,data)=>{
+                if(!err) spinner.succeed('设置成功')
+                else spinner.fail(err);
+            })
+        }
+        if(answers.npm_origin=='得物'){
+            child.exec('npm config set registry http://npm.poizon.com',(err,data)=>{
+                if(!err) spinner.succeed('设置成功')
+                else spinner.fail(err);
+            })
+        }
+    })
+}
