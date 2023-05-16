d2c18ece409f4c1abb578e0a1053bdf5735b8275
baize.zrj <18817313320@163.com>
2023-05-16 13:38:41 +0800

    📝

--- command/npm.js
+++ command/npm.js
@@ -1,34 +1,51 @@
 const child = require("child_process");
 const inquirer = require("inquirer");
 const spinner = require("ora")();
-module.exports = function () {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "npm_origin",
-        message: "选择镜像源?",
-        choices: ["npm原始", "淘宝"],
-      },
-    ])
-    .then((answers) => {
-      if (answers.npm_origin == "npm原始") {
-        child.exec(
-          "npm config set registry https://registry.npmjs.org/",
-          (err, data) => {
-            if (!err) spinner.succeed("设置成功 ");
-            else spinner.fail(err);
+const chalk = require("chalk");
+module.exports = function (type) {
+  if (type === "v") {
+    child.exec(
+      "npm get registry --global",
+      (err, data) => {
+        if (!err) {
+          if (data == "https://registry.npmmirror.com/") {
+            spinner.info("淘宝");
+          } else {
+            spinner.info("原始");
           }
-        );
+        }
+        else spinner.fail(err);
       }
-      if (answers.npm_origin == "淘宝") {
-        child.exec(
-          "npm config set registry https://registry.npmmirror.com/",
-          (err, data) => {
-            if (!err) spinner.succeed("设置成功");
-            else spinner.fail(err);
-          }
-        );
-      }
-    });
+    );
+  } else {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "npm_origin",
+          message: "选择镜像源?",
+          choices: ["npm原始", "淘宝"],
+        },
+      ])
+      .then((answers) => {
+        if (answers.npm_origin == "npm原始") {
+          child.exec(
+            "npm config set registry https://registry.npmjs.org/",
+            (err, data) => {
+              if (!err) spinner.succeed("设置成功 ");
+              else spinner.fail(err);
+            }
+          );
+        }
+        if (answers.npm_origin == "淘宝") {
+          child.exec(
+            "npm config set registry https://registry.npmmirror.com/",
+            (err, data) => {
+              if (!err) spinner.succeed("设置成功");
+              else spinner.fail(err);
+            }
+          );
+        }
+      });
+  }
 };
